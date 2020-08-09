ddaf35db52236c55bde3f079cb85b76c7b6653ef
David Galvis <ivan.galvis@project44.com>
2020-08-08 21:04:51 -0500

    Adding the support of jackson to read arrays and nested JSON objects

--- spring-rest/jackson-databind-json-processing/src/main/java/com/luv2code/jackson/json/demo/Driver.java
+++ spring-rest/jackson-databind-json-processing/src/main/java/com/luv2code/jackson/json/demo/Driver.java
@@ -16,12 +16,20 @@ public class Driver {
 			// data/sample-lite.json
 			//The following mapper code allow us to read code from the json file using the jackson dependency already set in the pom.xml
 			
-			Student theStudent = mapper.readValue(
-						new File("data/sample-lite.json"), Student.class);
+			//The following line just maps to the lite JSON file which does not have nested ojject neither arrays
+			//Student theStudent = mapper.readValue(new File("data/sample-lite.json"), Student.class);
+			
+			//Onthe other hand this line reads from the complex JSON object which uses an address object and an array of languages
+			//This is why that we have implemented the Address POJO and the languages array in the student class
+			Student theStudent = mapper.readValue(new File("data/sample-full.json"), Student.class);
+
+
+						
 			
 			// print first name and last name
 			System.out.println("First name = " + theStudent.getFirstName());
 			System.out.println("Last name = " + theStudent.getLastName());
+			System.out.println("City = " + theStudent.getAddress().getCity());
 
 		}
 		catch (Exception exc) {
